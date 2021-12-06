457406e50b9bb22c54e2174e975454722426231f
SabineZilde <sabine.zilde@protonmail.ch>
2021-12-06 16:03:28 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path='/:type/:id'>
+          <PetDetailsPage />
+        </Route>
+        <Route path='/:type?'>
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
